5e0347d7695fd91a22d327beda913fd6d7931835
Aleksandr Trebushchuk <zamsion@yandex.ru>
2020-03-02 00:04:07 +0700

    Output shape + input text

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 using namespace std;
 
 int main () {
-
+    char s[80];
+    string shape;
+    cout << "Enter a shape, set its values"<<endl;
+    cin.getline (s, 80);
+    for (int i = 0; s[i] != '\0'; i++) // Вывод данных фигуры
+        cout << s[i] ;
+    cout <<endl;
+    for (int i = 0; s[i] != ' '; i++) // Запоминаем название фигуры
+        shape +=s[i];
+    cout << shape <<endl;
     return 0;
 }
