523383478e6f7b6d184175a345f598c472ea1490
Aleksandr Trebushchuk <zamsion@yandex.ru>
2020-03-02 02:12:31 +0700

    end of part 1. output perimeter, area, input

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 //
 // Created by decider on 01.03.2020.
 //
-
+#include <cmath>
+#define _USE_MATH_DEFINES
 #include <iostream>
-using namespace std;
 
+using namespace std;
 int main () {
 
     char s[80];
-    float r, area;
+    float r;
     int k, x, y;
-    string shape, cr;
-    cout << "Enter a shape, set its values"<<endl;
+    string cr;
+    cout << "Enter a shape, set its values" <<endl;
     cin.getline (s, 80);
+    cout << "\n" << endl;
     for (int i = 0; s[i] != '\0'; i++) { // Вывод данных фигуры
         cout << s[i] ;
-        if (s[i] == ',') {
+        if (s[i] == ',') {     // Находим из массива радиус
             k = i+2;
             while (s[k] != ')') {
                 cr += s[k];
@@ -24,10 +26,8 @@ int main () {
         }
     }
     cout <<endl;
-    for (int i = 0; s[i] != '('; i++) // Запоминаем название фигуры
-        shape +=s[i];
     r = stof (cr);
-    cout << shape << " " << r<<endl;
+    cout << "  perimeter = " << 2 * M_PI * r<< "\n  area = " << M_PI * r * r <<endl;
 
     return 0;
 }
