345641805dda761cd04a2d47c6e0e2260289f5a8
Aleksandr Trebushchuk <zamsion@yandex.ru>
2020-03-07 12:48:22 +0700

    complete first part (circle), output x, y, area, perimeter

--- main.cpp
+++ main.cpp
@@ -6,28 +6,45 @@
 #include <iostream>
 
 using namespace std;
-int main () {
-
+int main()
+{
     char s[80];
     float r;
-    int k, x, y;
-    string cr;
-    cout << "Enter a shape, set its values" <<endl;
-    cin.getline (s, 80);
+    int x = 0, y = 0, i = 0;
+    string cr, shname, cx, cy;
+    cout << "Enter a shape, set its values" << endl;
+    cin.getline(s, 80);
     cout << "\n" << endl;
     for (int i = 0; s[i] != '\0'; i++) { // Вывод данных фигуры
-        cout << s[i] ;
-        if (s[i] == ',') {     // Находим из массива радиус
-            k = i+2;
-            while (s[k] != ')') {
-                cr += s[k];
-                k++;
-            }
+        cout << s[i];
+    }
+    while (s[i] != '(') {
+        shname += s[i];
+        i++;
+    }
+    if (shname == "circle") {
+        i = 7;
+        while (s[i] != ' ') {
+            cx += s[i];
+            i++;
+        }
+        i++;
+        while (s[i] != ',') {
+            cy += s[i];
+            i++;
+        }
+        i += 2;
+        while (s[i] != ')') {
+            cr += s[i];
+            i++;
         }
     }
-    cout <<endl;
-    r = stof (cr);
-    cout << "  perimeter = " << 2 * M_PI * r<< "\n  area = " << M_PI * r * r <<endl;
-
+    x = atoi(cx.c_str());
+    y = atoi(cy.c_str());
+    r = stof(cr);
+    cout << endl;
+    cout << "  x = " << x << "\n  y = " << y
+         << "\n  perimeter = " << 2 * M_PI * r << "\n  area = " << M_PI * r * r
+         << endl;
     return 0;
 }
