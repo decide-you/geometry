1c50848cf8f4eec65af0156944e6dbb94b3fc69c
Aleksandr Trebushchuk <zamsion@yandex.ru>
2020-05-16 18:48:54 +0700

    Fix Trouble #4

--- src/figurecheck.h
+++ src/figurecheck.h
@@ -1,4 +1,4 @@
 #ifndef FIGURECHECK_H
 #define FIGURECHECK_H
-void inpsh(char s[], int &x, int &x1, int &x2, int &x3, int &y, int &y1, int &y2, int &y3, float &r);
+void inpsh(char s[], int &x, int &x1, int &x2, int &x3, int &y, int &y1, int &y2, int &y3, int &r);
 #endif
